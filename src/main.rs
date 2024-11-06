7d1615e6b689afd0bba3f21b86240ef1d18f6585
Gordon McGaffin <gordon.t.mcgaffin@gmail.com>
2024-11-06 21:50:57 +0200

    Can read json files

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,6 @@ use utils::app_settings::AppSettings;
 
 fn main() {
     let mut app_settings = AppSettings::new();
+    print!("\nUsername is  {}\n",app_settings.get_username());
+    print!("Api key is {}\n",app_settings.get_api_key());
 }
--- src/utils/app_settings.rs
+++ src/utils/app_settings.rs
@@ -1,4 +1,15 @@
 use std::{fs::{self, File}, path::Path};
+use serde::{Deserialize, Serialize};
+use std::thread;
+use notify::{Watcher, RecommendedWatcher, RecursiveMode, Result};
+
+#[derive(Deserialize, Serialize, Debug)]
+struct Settings_File{
+    username: String,
+    api_key: String,
+    setting_3: u32,
+    setting_4: f64
+}
 
 pub struct AppSettings {
     username: String,
@@ -12,14 +23,48 @@ impl AppSettings {
             api_key: String::from("[PLACEHOLDER]"),
         };
         app_settings.load();
+        app_settings.start_watcher();
         app_settings
     }
 
     pub fn load(&mut self) {
         print!("Reading settings . . .\n");
-        let json_file_path = Path::new("./user_settings.json");
-        let mut settings_string = fs::read_to_string("user_settings.json").expect("Should have been able to read the file");
-        print!("{}\n", settings_string);
+        let mut settings: Settings_File = {
+            let data = fs::read_to_string("./user_settings/user_settings.json").expect("LogRocket: error reading file");
+            serde_json::from_str(&data).unwrap()
+        };
 
+        self.username = settings.username;
+        self.api_key = settings.api_key;
+    }
+
+    fn start_watcher(&mut self){
+        // thread::spawn(|| {
+        //     for i in 1..10 {
+
+        //         let mut watcher = notify::recommended_watcher(|res| {
+        //             match res {
+        //                Ok(event) => println!("event: {:?}", event),
+        //                Err(e) => println!("watch error: {:?}", e),
+        //             }
+        //         }).expect("Error setting up a watcher");
+
+                
+        //         watcher.watch(Path::new("./user_settings/user_settings.json"), RecursiveMode::Recursive).expect("Ran into error while watching");
+        //         print!("Files changed {}\n",i);
+        //     }
+        // }).join();
+        print!("\nThreading to be done\n");
+
+    }
+
+    // Add these getter methods to allow access to the private fields
+    // Thanks Chatgpt
+    pub fn get_username(&self) -> &str {
+        &self.username
+    }
+
+    pub fn get_api_key(&self) -> &str {
+        &self.api_key
     }
 }
